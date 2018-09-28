dc3a185b64e0e396a3fe3ba59f5450608a3b12e2
DESKTOP-KF35RC4\ASUS <shoaib.wattoo@gmail.com>
2018-09-28 12:22:35 +0500

    pushing code

---
+++ Assets/Scripts/EnemyAimScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAimScript : MonoBehaviour {
+
+	public Transform target; // target Object
+	public float enemyAimSpeed=5.0f; // Speed at Which Enenmy locks on the Target
+	Quaternion newRotation;
+
+	float orientTransform;
+	float orientTarget;
+	// Update is called once per frame
+	void Update () {
+		
+		orientTransform = transform.position.x; 
+		orientTarget = target.position.x;
+		// To Check on which side is the target , i.e. Right or Left of this Object
+		if (orientTransform > orientTarget) {
+			// Will Give Rotation angle , so that Arrow Points towards that target
+			newRotation = Quaternion.LookRotation (transform.position - target.position, -Vector3.up);	
+		} 
+		else {
+			newRotation = Quaternion.LookRotation (transform.position - target.position,Vector3.up);
+		}
+
+		// Here we have to freeze rotation along X and Y Axis, for proper movement of the arrow
+		newRotation.x = 0.0f;
+		newRotation.y = 0.0f;
+
+		// Finally rotate and aim towards the target direction using Code below
+			transform.rotation = Quaternion.Lerp (transform.rotation,newRotation,Time.deltaTime * enemyAimSpeed);
+
+
+		// Another Alternative
+		// transform.rotation = Quaternion.RotateTowards(transform.rotation,newRotation, Time.deltaTime * enemyAimSpeed);
+
+	}
+
+}
---
+++ Assets/Scripts/TargetMovementScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetMovementScript : MonoBehaviour {
+	public float targetSpeed=9.0f; // Speed At Which the Object Should Move
+
+	void Update () {
+		transform.Translate (Input.GetAxis ("Horizontal")*Time.deltaTime*targetSpeed,Input.GetAxis ("Vertical")*Time.deltaTime*targetSpeed,0);
+
+	}
+}
